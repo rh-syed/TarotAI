1668c4709c9691c1fb4594e53849df31bfe2e404
Syed <rh_syed@outlook.com>
2024-02-09 19:39:06 -0600

    DFS Tarot AI 3: Saving Chat In JSON File

--- tarot.py
+++ tarot.py
@@ -1,47 +1,54 @@
-import optparse
 from dotenv import load_dotenv, find_dotenv
-load_dotenv(find_dotenv(), override=True)
-
 from langchain_openai.chat_models import ChatOpenAI
 from langchain.schema import SystemMessage
 from langchain.chains import LLMChain
-from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
 
-#Memory 
-from langchain.memory import ConversationBufferMemory
-from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder
+# Memory
+from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
+
+
+from langchain.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder,
+)
+
+load_dotenv(find_dotenv(), override=True)
+
 
 llm = ChatOpenAI(model_name="gpt-3.5-turbo-0125", temperature=1)
 
-#create memory
+# create memory
+history = FileChatMessageHistory("tarot_history.json")
+
 memory = ConversationBufferMemory(
-    memory_key='chat_history',
-    return_messages=True
+    memory_key="chat_history", chat_memory=history, return_messages=True
 )
 
 # add MessagePlaceholder to the prompt
 prompt = ChatPromptTemplate(
-    input_variables = ["content"],
-    messages = [
-        SystemMessage(content = "You are a very intuitive, an expert psychic and accurate tarot card reader. Answer the users questions using 3 tarot cards (draw more cards if needed) and provide explanation, guidance and clarity they're seeking for."),
-        MessagesPlaceholder(variable_name = "chat_history"), #where the memeory will be stored
-        HumanMessagePromptTemplate.from_template("{content}")
-    ]
+    input_variables=["content"],
+    messages=[
+        SystemMessage(
+            content="You are a very intuitive, an expert psychic and accurate tarot card reader. Answer the users questions using 3 tarot cards (draw more cards if needed) and provide explanation, guidance and clarity they're seeking for."
+        ),
+        MessagesPlaceholder(
+            variable_name="chat_history"
+        ),  # where the memeory will be stored
+        HumanMessagePromptTemplate.from_template("{content}"),
+    ],
 )
 
-chain = LLMChain(
-    llm = llm,
-    prompt= prompt,
-    memory=memory,
-    verbose=False
-)
+chain = LLMChain(llm=llm, prompt=prompt, memory=memory, verbose=False)
 
 while True:
-    content = input('What is your question?: When you want to end the reading say "Thank you or Thanks" ')
-    if content.lower() in ['thank you', 'thanks'] :
-        print('Have a nice day!')
+    content = input(
+        'What is your question?: When you want to end the reading say "Thank you or Thanks" '
+    )
+    if content.lower() in ["thank you", "thanks"]:
+        print("Have a nice day!")
         break
-    print("Consulting Your Spirit Guides For Your Question " + content +" ....")
-    response = chain.run({'content' : content})
-    print (response)
-    print ('-' * 50)
+    print("Consulting Your Spirit Guides For Your Question " + content + " ....")
+    response = chain.run({"content": content})
+    print(response)
+    print("-" * 50)
